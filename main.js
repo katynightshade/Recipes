4b1d3c3a1737feea6c206027b4b8fd1687d81c3c
Katelyn Lindberg <katelynhouston@live.com>
2022-03-28 15:17:25 -0600

    attempt to refine menu buttons

--- main.js
+++ main.js
@@ -8,10 +8,12 @@ const navList = document.querySelector('.nav-list')
 menuOpen.addEventListener(('click'), () =>  {
     navigation.style.display = 'flex';
     navigation.style.flexDirection = 'column';
+    navigation.style.width = '95%';
     navList.style.listStyleType = 'none';
     header.style.display = 'flex';
     header.style.flexDirection = 'column';
     menuOpen.style.display = 'none';
+    menuExit.style.textAlign = 'left';
 });
 
 menuExit.addEventListener(('click'), () => {
